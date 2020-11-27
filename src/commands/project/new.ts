cebff52a37ec1f8067a7069183a8ae44edfc4196
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2020-11-27 20:58:42 +0000

    Implement project:new command

--- src/commands/project/new.ts
+++ src/commands/project/new.ts
@@ -1,4 +1,5 @@
 import { Command, flags } from "@oclif/command";
+import { GithubClient } from "../../project/github"; // TODO: Fix relative paths
 
 export default class ProjectNew extends Command {
   static description = "generate a new project";
@@ -7,11 +8,17 @@ export default class ProjectNew extends Command {
     help: flags.help({ char: "h" }),
   };
 
-  static args = [{ name: "name", required: true }];
+  static args = [{ name: "name", required: true }, { name: "description" }];
 
   async run() {
     const { args } = this.parse(ProjectNew);
 
-    console.log(args.name);
+    // TODO: Validate the GH_TOKEN
+
+    const githubClient = new GithubClient(process.env.GH_TOKEN as string);
+    const created = await githubClient.createRepo({ name: args.name });
+
+    this.log(`Created project ${created.fullName}`);
+    this.log(`Run: git clone ${created.sshUrl}`);
   }
 }
---
+++ src/project/github.ts
@@ -0,0 +1,36 @@
+import { Octokit } from "@octokit/rest";
+
+interface GithubRepo {
+  name: string;
+  fullName: string;
+  sshUrl: string;
+}
+
+interface CreateRepoOpts {
+  name: string;
+  description?: string;
+}
+
+export class GithubClient {
+  private octokitClient: Octokit;
+
+  constructor(apiKey: string) {
+    this.octokitClient = new Octokit({
+      auth: apiKey,
+    });
+  }
+
+  async createRepo({ name, description }: CreateRepoOpts): Promise<GithubRepo> {
+    const { data } = await this.octokitClient.repos.createForAuthenticatedUser({
+      name,
+      private: true,
+      description,
+    });
+
+    return {
+      name: data.name,
+      fullName: data.full_name,
+      sshUrl: data.ssh_url,
+    };
+  }
+}
